cc232192cbc8ce2eb22536671fc46c95ea595111
DBL <me@banli.co>
2025-04-25 16:01:41 +0800

    feat(StatsView): add sync and clear functionality with card display
    Introduce new features to StatsView including a sync button for iCloud integration, a clear button to delete all focus records, and a card view to display focus details. The sync functionality is currently commented out for future implementation.

--- EasyFocus/MVVM/Views/StatsView.swift
+++ EasyFocus/MVVM/Views/StatsView.swift
@@ -6,11 +6,56 @@
 //
 
 import SwiftUI
+import SwiftData
+import CoreData
 
 struct StatsView: View {
+  @Environment(\.modelContext) var context
+  @Query(sort: \Focus.createdAt, order: .reverse)
+  var focuses: [Focus] = []
+
   var body: some View {
-    VStack {
-      Text("Stats")
+    ScrollView {
+      HStack {
+        Button("Sync iCloud") {
+          sync()
+        }
+        Button("Clear All") {
+          Task {
+            focuses.forEach { context.delete($0) }
+          }
+        }
+      }
+      VStack {
+        ForEach(focuses.filter { $0.label != nil }) { focus in
+          CardView(focus)
+        }
+      }
+      .padding()
+    }
+  }
+  
+  func sync() {
+//    guard let container = context.container as? NSPersistentCloudKitContainer else { return }
+//        container.syncPersistentStores()
+  }
+  
+  @ViewBuilder
+  func CardView(_ focus: Focus) -> some View {
+    if let label = focus.label {
+      RoundedRectangle(cornerRadius: 12)
+        .fill(label.backgroundColor.isEmpty ? .white : Color(hex: label.backgroundColor))
+        .frame(height: 48)
+        .overlay {
+          HStack {
+            Text(label.name)
+              .foregroundColor(.white)
+            Spacer()
+            Text(Tools.formatSeconds(focus.completedSecondsCount))
+              .foregroundColor(.white)
+          }
+          .padding(.horizontal)
+        }
     }
   }
 }
