09be57490c03ea10abf8ea5dc10c6a7189b1ce64
DBL <me@banli.co>
2025-04-26 11:46:42 +0800

    refactor(ContentView): simplify view hierarchy and initialize tools
    Remove unused TabView and directly render FocusView. Add init method to set up transparent navigation and tab bars.

--- EasyFocus/ContentView.swift
+++ EasyFocus/ContentView.swift
@@ -12,16 +12,14 @@ struct ContentView: View {
   @EnvironmentObject var nav: NavKit
   @EnvironmentObject var stack: Stackit
   
+  init() {
+    Tools.transparentNavBar()
+    Tools.transparentTabBar()
+  }
+  
   var body: some View {
-    TabView(selection: $nav.activeNav) {
-//      StatsView()
-//        .tag(NavKit.NavType.stats)
-      FocusView()
-        .tag(NavKit.NavType.focus)
-//      SettingsView()
-//        .tag(NavKit.NavType.settings)
-    }
-    .tabViewStyle(.page)
+    FocusView()
+      .tag(NavKit.NavType.focus)
   }
 }
 
