393d427643a967a2106a0f8a64d6b1a078373d5b
DBL <me@banli.co>
2025-04-26 15:24:12 +0800

    refactor(StatsView): remove iCloud sync button and adjust focus height dynamically
    The iCloud sync button was removed as it was not functional. The height of focus items is now dynamically adjusted based on the duration of the focus session, improving the visual representation of the data.

--- EasyFocus/MVVM/Views/StatsView.swift
+++ EasyFocus/MVVM/Views/StatsView.swift
@@ -21,9 +21,6 @@ struct StatsView: View {
         BackButton("sf.xmark")
       }
       HStack {
-        Button("Sync iCloud") {
-          sync()
-        }
         Button("Clear All") {
           Task {
             focuses.forEach { context.delete($0) }
@@ -39,9 +36,22 @@ struct StatsView: View {
     }
   }
   
-  func sync() {
-//    guard let container = context.container as? NSPersistentCloudKitContainer else { return }
-//        container.syncPersistentStores()
+  func getDynamicHeight(_ seconds: Int) -> CGFloat{
+    let minutes = seconds / 60
+    if minutes < 5 {
+      return 48
+    } else if (5..<10).contains(minutes) {
+      return 60
+    } else if (10..<25).contains(minutes) {
+      return 80
+    } else if (25..<50).contains(minutes) {
+      return 100
+    } else if (50..<100).contains(minutes) {
+      return 120
+    } else {
+      return 140
+    }
+    
   }
   
   @ViewBuilder
@@ -49,18 +59,21 @@ struct StatsView: View {
     if let label = focus.label {
       RoundedRectangle(cornerRadius: 12)
         .fill(label.backgroundColor.isEmpty ? .white : Color(hex: label.backgroundColor))
-        .frame(height: 48)
+        .frame(height: getDynamicHeight(focus.completedSecondsCount))
         .overlay {
-          HStack {
-            Text(label.name)
-              .foregroundColor(.white)
+          VStack {
+            HStack {
+              Text(label.name)
+                .foregroundColor(.white)
+              Spacer()
+              Text(Tools.format(focus.createdAt))
+                .foregroundColor(.white)
+              Text(Tools.formatSeconds(focus.completedSecondsCount))
+                .foregroundColor(.white)
+            }
+            .padding()
             Spacer()
-            Text(Tools.format(focus.createdAt))
-              .foregroundColor(.white)
-            Text(Tools.formatSeconds(focus.completedSecondsCount))
-              .foregroundColor(.white)
           }
-          .padding(.horizontal)
         }
     }
   }
