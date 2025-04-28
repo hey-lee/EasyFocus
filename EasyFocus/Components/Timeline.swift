dde3e6cbe72f20dfe6b375d57dfd89477fb63155
DBL <me@banli.co>
2025-04-28 12:51:41 +0800

    feat: add `Timeline` component to display focus events
    Introduce a new `Timeline` component to visualize focus events in a scrollable timeline format. Each event is represented by a `TimelineItem` that includes details such as time range, duration, and optional notes. This enhances the user interface by providing a clear overview of focus sessions.

---
+++ EasyFocus/Components/Timeline.swift
@@ -0,0 +1,119 @@
+//
+//  TimelineView.swift
+//  EasyFocus
+//
+//  Created by DBL on 2025/4/28.
+//
+
+import SwiftUI
+
+struct Timeline: View {
+  let events: [Focus]
+  
+  init(_ events: [Focus] = []) {
+    self.events = events
+  }
+  
+  var body: some View {
+    ScrollView {
+      VStack(spacing: 0) {
+        ForEach(Array(events.enumerated()), id: \.element.id) { index, event in
+          TimelineItem(event, isLast: index == events.count - 1)
+        }
+      }
+      .padding()
+    }
+  }
+}
+
+struct TimelineItem: View {
+  let event: Focus
+  var isLast: Bool = false
+  
+  init(_ event: Focus, isLast: Bool) {
+    self.event = event
+    self.isLast = isLast
+  }
+  
+  var body: some View {
+    HStack(alignment: .top, spacing: 0) {
+      VStack(spacing: 0) {
+        let ringSize: CGFloat = 12
+        Circle()
+          .stroke(event.completedSecondsCount == event.minutes * FocusKit.shared.minuteInSeconds ? .black : .red, lineWidth: 2)
+          .frame(width: ringSize, height: ringSize)
+          .zIndex(1)
+        
+        if !isLast {
+          Rectangle()
+            .stroke(Color.slate200, style: .init(lineWidth: 1, dash: [10]))
+            .frame(width: 0.5)
+            .frame(maxHeight: .infinity)
+        }
+      }
+      .frame(width: 30)
+      
+      VStack(alignment: .leading, spacing: 8) {
+        HStack {
+          Text("\(event.startedAt.format("HH:mm"))-\(event.endedAt.format("HH:mm"))")
+            .font(.system(size: 16, weight: .semibold))
+          
+          Spacer()
+          
+          HStack(spacing: 4) {
+            Text(Tools.formatSeconds(event.completedSecondsCount))
+              .foregroundColor(.secondary)
+          }
+        }
+        .offset(y: -4)
+        
+        if let label = event.label {
+          VStack(alignment: .leading, spacing: 12) {
+            HStack {
+              Text(label.name)
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(.black)
+              
+              Spacer()
+              
+              Button() {
+                //
+              } label: {
+                Image(systemName: "square.and.pencil")
+                  .foregroundColor(.gray)
+              }
+            }
+            
+            Text(event.notes)
+              .foregroundColor(.gray)
+              .font(.system(size: 16))
+          }
+          .padding(12)
+          //          .background(Color(hex: label.backgroundColor))
+          .background(Color.slate50)
+          .cornerRadius(16)
+        }
+        
+        Spacer()
+          .frame(height: 40)
+      }
+    }
+  }
+}
+
+import SwiftData
+struct TimelinePreviewView: View {
+  @Query var events: [Focus]
+  
+  var body: some View {
+    Timeline(events)
+  }
+}
+
+#Preview {
+  TimelinePreviewView()
+    .modelContainer(for: [
+      Focus.self,
+      FocusLabel.self,
+    ])
+}
