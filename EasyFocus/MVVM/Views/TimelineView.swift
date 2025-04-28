61cc46de1c94dabdeb96671d8a332282dbc99c63
DBL <me@banli.co>
2025-04-28 13:08:34 +0800

    feat(TimelineView): filter events by selected date
    Add functionality to filter and display events based on the selected date in the TimelineView. This improves user experience by showing only relevant events for the chosen day.

--- EasyFocus/MVVM/Views/TimelineView.swift
+++ EasyFocus/MVVM/Views/TimelineView.swift
@@ -10,7 +10,10 @@ import SwiftData
 
 struct TimelineView: View {
   @Query var events: [Focus]
+  @Environment(\.modelContext) var context
+
   @State var date: Date = Date()
+
   var body: some View {
     VStack {
       HStack {
@@ -25,12 +28,31 @@ struct TimelineView: View {
           .fontWeight(.semibold)
           .textScale(.secondary)
           .foregroundStyle(.gray)
-
+        
         Spacer()
       }
       .padding(.horizontal)
-      Timeline(events)
+      Timeline(getEvents(by: date))
     }
+  }
+  
+  private func getEvents(by date: Date) -> [Focus] {
+    var events: [Focus] = []
+    let calendar = Calendar.current
+    let startOfDay = calendar.startOfDay(for: date)
+    let endOfDay = calendar.date(byAdding: .day, value: 1, to: startOfDay)!
+    let predicate = #Predicate<Focus> {
+      $0.createdAt >= startOfDay && $0.createdAt < endOfDay
+    }
+    let descriptor = FetchDescriptor<Focus>(predicate: predicate)
+    
+    do {
+      events = try context.fetch(descriptor)
+    } catch {
+      print("getAuthUser fail: \(error)")
+    }
+    
+    return events
   }
 }
 
