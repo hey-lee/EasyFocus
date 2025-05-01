bff99179bc3b5701eb51b21bc1267be58dc43c07
DBL <me@banli.co>
2025-05-01 12:58:29 +0800

    refactor(StoreKit): improve data handling and add mock functionality
    Refactor the StoreKit class to enhance data handling by setting a default value for `rangeType` and computing `focusEvents` dynamically. Add a `mock` method to generate random focus events for testing and demonstration purposes. Simplify and optimize label-related computations and introduce `chartEntities` for better data visualization.

--- EasyFocus/MVVM/ViewModels/StoreKit.swift
+++ EasyFocus/MVVM/ViewModels/StoreKit.swift
@@ -16,8 +16,10 @@ final class StoreKit {
     var description: String { rawValue }
   }
   
-  var rangeType: String = ""
-  var focusEvents: [Focus] = []
+  var rangeType: String = "day"
+  var focusEvents: [Focus] {
+    StoreKit.mock(100)
+  }
   var totalSeconds: Int {
     focusEvents.reduce(0) { $0 + $1.completedSecondsCount }
   }
@@ -53,22 +55,25 @@ final class StoreKit {
       }
     }
   }
-  var labelStats: [FocusLabel: [Focus]] {
-    rangedEvents
-      .filter { $0.label != nil }
-      .reduce(into: [:]) { dict, focus in
-        if let label = focus.label {
-          dict[label, default: []].append(focus)
-        }
-      }
-  }
   var labelEventsMap: [String: [Focus]] {
-    Dictionary(grouping: focusEvents.filter { $0.label != nil }) { $0.label!.name }
+    Dictionary(grouping: rangedEvents.filter { $0.label != nil }) { $0.label!.name }
   }
-  var labelValueMap: [String: Int] {
+  var labelValueMap: [(key: String, value: Int)] {
     labelEventsMap.mapValues { events in
       events.reduce(0) { $0 + $1.completedSecondsCount }
     }
+    .sorted { $0.value > $1.value }
+  }
+  
+  var chartEntities: [ChartEntity] {
+    labelValueMap.map { item in
+      ChartEntity(
+        label: item.key,
+        value: toMinutes(item.value),
+        percent: percent(item.value)
+      )
+    }
+    .filter { $0.percent > 0 }
   }
   
   func percent(_ seconds: Int) -> Int {
@@ -77,5 +82,44 @@ final class StoreKit {
   
   func toMinutes(_ seconds: Int) -> Int {
     Int(Double(seconds) / 60.0)
+  }
+  
+  static func mock(
+    _ count: Int,
+    range: ClosedRange<Date> = Date().addingTimeInterval(-30 * 24 * 3600)...Date()
+  ) -> [Focus] {
+    var events = [Focus]()
+    
+    let labels: [FocusLabel] = [
+      .init(id: "study", name: "Study", backgroundColor: "#22c55e"),
+      .init(id: "work", name: "Work", backgroundColor: "#fde047"),
+      .init(id: "sport", name: "Sport", backgroundColor: "#2563eb"),
+      .init(id: "reading", name: "Reading", backgroundColor: "#c4b5fd"),
+      .init(id: "writing", name: "Writing", backgroundColor: "#fda4af"),
+      .init(id: "meditation", name: "Meditation", backgroundColor: "#fdba74"),
+      .init(id: "workout", name: "Workout", backgroundColor: "#d8b4fe"),
+    ]
+    
+    // gen random events
+    for _ in 0..<count {
+      let randomDate = Date(timeIntervalSinceNow: Double.random(in: range.lowerBound.timeIntervalSinceNow..<range.upperBound.timeIntervalSinceNow))
+      let minutes = Int.random(in: 20..<50)
+      let sessionsCount = Int.random(in: 4..<8)
+      let completedSecondsCount = sessionsCount * minutes * 60
+
+      let event = Focus(
+        minutes: minutes,
+        sessionsCount: sessionsCount,
+        completedSecondsCount: completedSecondsCount,
+        label: labels.randomElement(),
+        startedAt: randomDate,
+        endedAt: randomDate.addingTimeInterval(TimeInterval(completedSecondsCount)),
+        createdAt: randomDate,
+      )
+      
+      events.append(event)
+    }
+    
+    return events
   }
 }
