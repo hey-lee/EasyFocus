dc487b4d9c5c84071f12cda81cf8b5d5e78eeeed
DBL <me@banli.co>
2025-05-05 17:03:28 +0800

    feat(timer): add `TimerView` for managing focus and rest sessions
    Introduce a new SwiftUI view, TimerView, to handle focus and rest sessions. The view includes buttons for starting, pausing, resuming, and stopping the timer, as well as displaying timer state, settings, and session details. This addition enhances the user experience by providing a centralized interface for timer management.

---
+++ EasyFocus/MVVM/Views/TimerView.swift
@@ -0,0 +1,88 @@
+//
+//  TimerView.swift
+//  EasyFocus
+//
+//  Created by DBL on 2025/5/3.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+  @Environment(FocusService.self) var focusService
+  
+  var body: some View {
+    PageView {
+      HStack(spacing: 8) {
+        Group {
+          Text(focusService.display.minutes)
+          VStack {
+            Circle()
+              .frame(width: 20, height: 20)
+            Circle()
+              .frame(width: 20, height: 20)
+          }
+          Text(focusService.display.seconds)
+        }
+        .tracking(-4)
+        .font(.custom("Code Next ExtraBold", size: UIDevice.current.orientation.isLandscape ? 200 : 100).monospacedDigit())
+      }
+      
+      Group {
+        HStack {
+          Button("Start Work") {
+            focusService.start()
+          }
+          Button("Start Rest") {
+            focusService.start()
+          }
+          Button("Pause") {
+            focusService.pause()
+          }
+          Button("Resume") {
+            focusService.resume()
+          }
+          Button("Stop") {
+            focusService.stop()
+          }
+        }
+        HStack {
+          Button("Request Notification") {
+            Task {
+              _ = await NotificationService.shared.requestAuthorization()
+            }
+          }
+        }
+        VStack(alignment: .leading) {
+          Text("Timer state: \(focusService.sm.state)")
+          Text("Timer minutes: \(focusService.settings.minutes)")
+          Text("Timer seconds: \(focusService.duration) (\(focusService.sm.mode))")
+          Text("Timer break type: \(focusService.breakType)")
+          Text("Short break minutes: \(focusService.settings.shortBreak)")
+          Text("Long break minutes: \(focusService.settings.longBreak)")
+          Text("Total sessions count: \(focusService.settings.sessionsCount)")
+          Text("Completed cessions count: \(focusService.completedSessionsCount)")
+          Text("Remaining seconds: \(focusService.timer.remainingSeconds)")
+          Toggle("Auto start sessions", isOn: .init(get: {
+            focusService.settings.autoStartSessions
+          }, set: { value in
+            focusService.settings.autoStartSessions = value
+          }))
+          Toggle("Auto start short break", isOn: .init(get: {
+            focusService.settings.autoStartShortBreaks
+          }, set: { value in
+            focusService.settings.autoStartShortBreaks = value
+          }))
+        }
+      }
+      .buttonStyle(.borderedProminent)
+    }
+    .onChange(of: focusService.duration) { oldValue, newValue in
+      print("focusService.duration", newValue)
+    }
+  }
+}
+
+#Preview {
+  TimerView()
+    .environment(FocusService())
+}
