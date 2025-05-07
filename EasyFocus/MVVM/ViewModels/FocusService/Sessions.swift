9deb10316ce46da7653128427359851c768ea100
DBL <me@banli.co>
2025-05-07 13:45:18 +0800

    refactor(Sessions): remove unnecessary access modifiers and add `totalSeconds` and completedSeconds properties
    Simplify the code by removing redundant public access modifiers and introduce new properties to calculate total and completed session time in seconds.

--- EasyFocus/MVVM/ViewModels/FocusService/Sessions.swift
+++ EasyFocus/MVVM/ViewModels/FocusService/Sessions.swift
@@ -19,31 +19,37 @@ extension FocusService {
   final class Sessions {
     static let shared = Sessions()
     
-    public var progress: Double = 0
-    public var breakType: BreakType {
+    var progress: Double = 0
+    var breakType: BreakType {
       isComplete ? .long : .short
     }
-    public var isComplete: Bool {
+    var isComplete: Bool {
       completedCount == totalCount
     }
-    public var totalCount: Int {
+    var totalCount: Int {
       Settings.shared.sessionsCount
     }
-    public var completedCount: Int = 0
-    
-    public func finish() {
-      completedCount = min(completedCount + 1, totalCount)
+    var completedCount: Int = 0
+    var totalSeconds: Int {
+      Settings.shared.minutes * FocusService.shared.ONE_MINUTE_IN_SECONDS
+    }
+    var completedSeconds: Int {
+      completedCount * totalSeconds
     }
     
-    public func restore() {
+    func finish() {
+      completedCount = max(min(completedCount + 1, totalCount), 0)
+    }
+    
+    func restore() {
       completedCount = 0
     }
     
-    public func getPendingCount(by mode: StateMachine.Mode) -> Int {
+    func getPendingCount(by mode: StateMachine.Mode) -> Int {
       totalCount - completedCount - (mode == .work ? 1 : 0)
     }
     
-    public func getSessionProgress(_ index: Int, _ mode: StateMachine.Mode) -> CGFloat {
+    func getSessionProgress(_ index: Int, _ mode: StateMachine.Mode) -> CGFloat {
       completedCount > index ? 1 : ((completedCount == index) && mode == .work ? progress : 0)
     }
   }
