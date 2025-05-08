1bd8dd794e98539ef24b5338743a96f28ea4392e
Lee <me@banli.co>
2025-05-08 18:11:45 +0800

    refactor(TimerView): simplify timer controls and improve UI layout
    - Replace separate buttons for Start, Pause, Resume, and Stop with a single dynamic control
    - Remove redundant notification request button and debug information
    - Introduce a Row helper function for consistent layout of timer details

--- EasyFocus/MVVM/Views/TimerView.swift
+++ EasyFocus/MVVM/Views/TimerView.swift
@@ -12,7 +12,7 @@ struct TimerView: View {
   
   var body: some View {
     PageView {
-      HStack(spacing: 8) {
+      HStack {
         Group {
           Text(focusService.display.minutes)
           VStack {
@@ -29,42 +29,37 @@ struct TimerView: View {
       
       Group {
         HStack {
-          Button("Start") {
-            focusService.start()
+          let symbolName = switch focusService.sm.state {
+          case .idle, .paused: "play.fill"
+          case .running: "pause.fill"
           }
-          Button("Pause") {
-            focusService.pause()
-          }
-          Button("Resume") {
-            focusService.resume()
-          }
-          Button("Stop") {
-            focusService.stop()
-          }
-        }
-        HStack {
-          Button("Request Notification") {
-            Task {
-              _ = await NotificationService.shared.requestAuthorization()
+          Symbol("sf.\(symbolName)", colors: [.slate200], contentSize: .small)
+            .onTapGesture {
+              withAnimation {
+                switch focusService.sm.state {
+                case .idle:
+                  focusService.start()
+                case .running:
+                  focusService.pause()
+                case .paused:
+                  focusService.resume()
+                }
+              }
             }
-          }
+          Symbol("sf.stop.fill", colors: [.slate200], contentSize: .small)
+            .onTapGesture {
+              withAnimation {
+                focusService.stop()
+              }
+            }
+//          Symbol("sf.bell.badge.fill", colors: [.slate200], contentSize: .small)
+//            .onTapGesture {
+//              Task {
+//                _ = await NotificationService.shared.requestAuthorization()
+//              }
+//            }
         }
-        VStack(alignment: .leading) {
-          Text("Timer state: \(focusService.sm.state)")
-          Text("Timer minutes: \(focusService.settings.minutes)")
-          Text("Timer seconds: \(focusService.duration) (\(focusService.sm.mode))")
-          Text("Timer break type: \(focusService.sessions.breakType)")
-          Text("Short break minutes: \(focusService.settings.shortBreakMinutes)")
-          Text("Long break minutes: \(focusService.settings.longBreakMinutes)")
-          Text("Total sessions count: \(focusService.sessions.totalCount)")
-          Text("Completed cessions count: \(focusService.sessions.completedCount)")
-          Text("Remaining seconds: \(focusService.timer.remainingSeconds)")
-          Text("Total seconds: \(focusService.seconds.total)")
-          Text("Total remaining seconds: \(focusService.totalRemainingSeconds)")
-          Text("Schedule seconds: \(focusService.scheduleSeconds)")
-
-          Text("Background seconds: \(focusService.seconds.background)s")
-
+        VStack(alignment: .leading, spacing: 8) {
           Toggle("Auto start sessions", isOn: .init(get: {
             focusService.settings.autoStartSessions
           }, set: { value in
@@ -75,12 +70,70 @@ struct TimerView: View {
           }, set: { value in
             focusService.settings.autoStartShortBreaks = value
           }))
+          Row("Timer state") {
+            Text("\(focusService.sm.state)")
+          }
+          Row("Timer minutes") {
+            Text("\(focusService.settings.minutes)")
+          }
+          Row("Timer seconds") {
+            Text("\(focusService.duration) (\(focusService.sm.mode))")
+          }
+          Row("Timer break type") {
+            Text("\(focusService.sessions.breakType)")
+          }
+          Row("Short break minutes") {
+            Text("\(focusService.settings.shortBreakMinutes)")
+          }
+          Row("Long break minutes") {
+            Text("\(focusService.settings.longBreakMinutes)")
+          }
+          Row("Total sessions count") {
+            Text("\(focusService.sessions.totalCount)")
+          }
+          Row("Completed cessions count") {
+            Text("\(focusService.sessions.completedCount)")
+          }
+          Row("Remaining seconds") {
+            Text("\(focusService.timer.remainingSeconds)")
+          }
+          Row("Total seconds") {
+            Text("\(focusService.seconds.total)")
+          }
+          Row("Total remaining seconds") {
+            Text("\(focusService.totalRemainingSeconds)")
+          }
+          Row("Schedule seconds") {
+            Text("\(focusService.scheduleSeconds)")
+          }
+          Row("Background seconds") {
+            Text("\(focusService.seconds.background)s")
+          }
         }
       }
       .buttonStyle(.borderedProminent)
     }
     .onChange(of: focusService.mode) { oldValue, newValue in
       print("mode", newValue)
+    }
+  }
+  
+  @ViewBuilder
+  func Row(_ label: String, @ViewBuilder value: @escaping () -> some View = { EmptyView() }) -> some View {
+    let width = UIScreen.main.bounds.width - 32
+    let leftWidth = width / 3 * 2
+    let rightWidth = width / 3
+    HStack {
+      HStack {
+        Text(label)
+        Spacer()
+      }
+      .frame(width: leftWidth)
+      HStack {
+        value()
+        Spacer()
+      }
+      .frame(width: rightWidth)
     }
   }
 }
