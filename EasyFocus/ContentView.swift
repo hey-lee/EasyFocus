9019d530992063f26497d2ea26f7264f2420c45c
DBL <me@banli.co>
2025-05-11 13:28:28 +0800

    feat: add timer button to trigger TimerView overlay
    Introduce a new state variable `showTimerView` and a timer button in the `ContentView` overlay. When tapped, the button presents the `TimerView` as a sheet. This enhancement allows users to access the timer functionality directly from the main view.

--- EasyFocus/ContentView.swift
+++ EasyFocus/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftData
 struct ContentView: View {
   @EnvironmentObject var nav: NavKit
   @EnvironmentObject var stack: Stackit
+  @State var showTimerView: Bool = false
   
   init() {
     Tools.transparentNavBar()
@@ -19,7 +20,23 @@ struct ContentView: View {
   
   var body: some View {
     FocusView()
-      .tag(NavKit.NavType.focus)
+      .overlay {
+        VStack {
+          Spacer()
+          HStack {
+            Spacer()
+            Symbol("sf.timer", colors: [.slate50])
+              .onTapGesture {
+                showTimerView = true
+              }
+          }
+          .padding()
+          .padding(.bottom, 40)
+        }
+      }
+      .sheet(isPresented: $showTimerView) {
+        TimerView()
+      }
   }
 }
 
