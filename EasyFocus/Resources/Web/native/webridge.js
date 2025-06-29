96753a1015453590780f28d4784d4d496d5f1bbb
DBL <me@banli.co>
2025-06-29 20:42:09 +0800

    feat(stats): add month stats and improve bridge functionality
    - Add month stats button and handler similar to year stats
    - Implement callScheme method in webridge for better URL handling
    - Update stats display to show both label and value
    - Move bridge exports to dedicated bridge module

--- EasyFocus/Resources/Web/native/webridge.js
+++ EasyFocus/Resources/Web/native/webridge.js
@@ -1,3 +1,49 @@
+function _array_like_to_array(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+    for(var i = 0, arr2 = new Array(len); i < len; i++)arr2[i] = arr[i];
+    return arr2;
+}
+function _array_with_holes(arr) {
+    if (Array.isArray(arr)) return arr;
+}
+function _iterable_to_array_limit(arr, i) {
+    var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"];
+    if (_i == null) return;
+    var _arr = [];
+    var _n = true;
+    var _d = false;
+    var _s, _e;
+    try {
+        for(_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true){
+            _arr.push(_s.value);
+            if (i && _arr.length === i) break;
+        }
+    } catch (err) {
+        _d = true;
+        _e = err;
+    } finally{
+        try {
+            if (!_n && _i["return"] != null) _i["return"]();
+        } finally{
+            if (_d) throw _e;
+        }
+    }
+    return _arr;
+}
+function _non_iterable_rest() {
+    throw new TypeError("Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _sliced_to_array(arr, i) {
+    return _array_with_holes(arr) || _iterable_to_array_limit(arr, i) || _unsupported_iterable_to_array(arr, i) || _non_iterable_rest();
+}
+function _unsupported_iterable_to_array(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _array_like_to_array(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(n);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _array_like_to_array(o, minLen);
+}
 var __defProp = Object.defineProperty;
 var __export = function(target, all) {
     for(var name in all)__defProp(target, name, {
@@ -5,9 +51,12 @@ var __export = function(target, all) {
         enumerable: true
     });
 };
-// src/fns/index.ts
-var fns_exports = {};
-__export(fns_exports, {
+// src/bridge/index.ts
+var bridge_exports = {};
+__export(bridge_exports, {
+    callScheme: function() {
+        return callScheme;
+    },
     emit: function() {
         return emit;
     },
@@ -15,6 +64,25 @@ __export(fns_exports, {
         return invoke;
     }
 });
+// src/fns/index.ts
+var stringify = function() {
+    var params = arguments.length > 0 && arguments[0] !== void 0 ? arguments[0] : {};
+    var queryString = Object.entries(params).map(function(param) {
+        var _param = _sliced_to_array(param, 2), key = _param[0], value = _param[1];
+        return "".concat(encodeURIComponent(key), "=").concat(encodeURIComponent(value));
+    }).join("&");
+    return Object.keys(params).length ? "?".concat(queryString) : queryString;
+};
+// src/bridge/index.ts
+var callScheme = function(action) {
+    var params = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : {};
+    try {
+        var url = "foca://".concat(action).concat(stringify(params));
+        window.location.href = url;
+    } catch (error) {
+        console.log("callScheme error:", error);
+    }
+};
 var invoke = function(action) {
     var params = arguments.length > 1 && arguments[1] !== void 0 ? arguments[1] : {};
     try {
@@ -42,6 +110,5 @@ var emit = function(key, data) {
     }
 };
 // src/index.ts
-window.bridge = fns_exports;
-export { emit, invoke };
+window.bridge = bridge_exports;
 //# sourceMappingURL=webridge.js.map
